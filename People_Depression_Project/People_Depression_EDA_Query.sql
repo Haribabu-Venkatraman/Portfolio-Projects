584cc4f30792a7fe325b5dcc7f82675c0b6775da
Haribabu Venkatraman <harivenkatraman5@gmail.com>
2024-08-10 21:17:47 +0530

    update

--- People_Depression_Project/People_Depression_EDA_Query.sql
+++ People_Depression_Project/People_Depression_EDA_Query.sql
@@ -4,6 +4,7 @@
 use People_Depression_database;
 
 
+
 -- Total number of people based on Age category:
 
 select *
@@ -47,7 +48,7 @@ set Age_Bucket = (Case when age < 21 then 'Under 20'
 	END)
 
 
-Select count(*)
+Select *
 from depression_staged_main
 
 -- Total number of People based Age Bucket:
@@ -73,5 +74,180 @@ from Percent_age_cte
 order by age_bucket
 
 
+-- Total people by Marital_Status:
+
+select Marital_Status, count(Marital_Status) Count_Marital
+from depression_staged_main
+group by Marital_Status
+order by Count_Marital;
+
+-- Percent of total people depressed by Marital_Status:
+
+with Percent_ms as
+(
+Select Marital_Status, 
+	   count(Marital_Status) Count_Marital
+from depression_staged_main
+group by Marital_Status
+)
+select Marital_Status, Count_Marital, round(cast(Count_Marital as float)/cast(sum(Count_Marital) over () as float),3) as Percent_Marital_Status
+from Percent_ms
+order by Marital_Status
 
 
+-- Employeed Vs Unemployeed:
+
+select Employment_Status, count(Employment_Status) Count_Emp
+from depression_staged_main
+group by Employment_Status
+order by Count_Emp desc,Employment_Status
+
+
+-- Employment Status and Education Level:
+
+select Education_Level,Employment_Status, count(Education_Level) Count_Ed_Emp
+from depression_staged_main
+group by Education_Level,Employment_Status
+order by Count_Ed_Emp desc,Employment_Status
+
+
+with percent_Edu_Emp as
+(
+select Education_Level,
+	   Employment_Status, 
+	   count(Education_Level) Count_Ed_Emp
+from depression_staged_main
+group by Education_Level,Employment_Status
+)
+select Education_Level,
+	   Employment_Status,
+	   Count_Ed_Emp,
+	   round(cast(Count_Ed_Emp as float)/cast(sum(Count_Ed_Emp) over () as float),3) as Percent_Ed_Emp
+from percent_Edu_Emp
+order by Count_Ed_Emp desc,Employment_Status
+
+
+-- Graduates with Job vs Graduates without Job :
+
+-- Graduates with No Job:
+
+with grad_njb as
+(
+select Education_Level,
+	   Employment_Status, 
+	   count(Education_Level) Count_Ed_unEmp
+from depression_staged_main
+where Employment_Status like 'Unemployed'
+group by Education_Level,Employment_Status
+),
+rolling_sum_grad_njb as
+(
+select *,
+	  sum(Count_Ed_unEmp) over(partition by Employment_Status order by Education_Level) rolling_sum_unEmp
+from grad_njb
+),
+percent_grad_njb as
+(
+select *,
+	  round(cast(rolling_sum_unEmp as float)/cast(sum(Count_Ed_unEmp) over (partition by Employment_Status) as float),3) as Percent_Grad_njb
+from rolling_sum_grad_njb
+),
+
+-- Graduates with Job:
+
+grad_jb as
+(
+select Education_Level,
+	   Employment_Status, 
+	   count(Education_Level) Count_Ed_Emp
+from depression_staged_main
+where Employment_Status like 'Employed'
+group by Education_Level,Employment_Status
+),
+rolling_sum_grad_jb as
+(
+select *,
+	  sum(Count_Ed_Emp) over(partition by Employment_Status order by Education_Level) rolling_sum_emp
+from grad_jb
+),
+percent_grad_jb as
+(
+select *,
+	  round(cast(rolling_sum_emp as float)/cast(sum(Count_Ed_Emp) over (partition by Employment_Status) as float),3) as Percent_Grad_jb
+from rolling_sum_grad_jb
+)
+
+select 
+	   case when a.Education_Level is not null then a.Education_Level else b.Education_Level end as Education_Level,
+       a.Employment_Status as Employment_Status_01,
+	   a.Count_Ed_unEmp,
+	   a.rolling_sum_unEmp,
+	   a.Percent_Grad_njb,
+	   b.Employment_Status as Employment_Status_02, 
+	   b.Count_Ed_Emp,
+	   b.rolling_sum_emp,
+	   b.Percent_Grad_jb
+from percent_grad_njb a
+full outer join percent_grad_jb b
+on a.education_level = b.education_level
+
+-- Using Union
+
+with grad_njb as
+(
+select Education_Level,
+	   Employment_Status, 
+	   count(Education_Level) Count_Ed_unEmp
+from depression_staged_main
+where Employment_Status like 'Unemployed'
+group by Education_Level,Employment_Status
+),
+rolling_sum_grad_njb as
+(
+select *,
+	  sum(Count_Ed_unEmp) over(partition by Employment_Status order by Education_Level) rolling_sum_unEmp
+from grad_njb
+),
+percent_grad_njb as
+(
+select *,
+	  round(cast(rolling_sum_unEmp as float)/cast(sum(Count_Ed_unEmp) over (partition by Employment_Status) as float),3) as Percent_Grad_njb
+from rolling_sum_grad_njb
+),
+
+-- Graduates with Job:
+
+grad_jb as
+(
+select Education_Level,
+	   Employment_Status, 
+	   count(Education_Level) Count_Ed_Emp
+from depression_staged_main
+where Employment_Status like 'Employed'
+group by Education_Level,Employment_Status
+),
+rolling_sum_grad_jb as
+(
+select *,
+	  sum(Count_Ed_Emp) over(partition by Employment_Status order by Education_Level) rolling_sum_emp
+from grad_jb
+),
+percent_grad_jb as
+(
+select *,
+	  round(cast(rolling_sum_emp as float)/cast(sum(Count_Ed_Emp) over (partition by Employment_Status) as float),3) as Percent_Grad_jb
+from rolling_sum_grad_jb
+)
+
+select *
+from percent_grad_njb
+union all
+select *
+from percent_grad_jb
+
+
+select distinct (Income), count(Income) countt
+from depression_staged_main
+group  by income
+having count(Income) > 1
+order by countt desc
